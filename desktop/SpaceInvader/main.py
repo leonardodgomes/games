93c5045f0a925c367c7ef511a3113e976bf5072b
leonardodgomes <43643753+leonardodgomes@users.noreply.github.com>
2022-04-27 14:44:01 +0100

    The basic screen was created

--- desktop/SpaceInvader/main.py
+++ desktop/SpaceInvader/main.py
@@ -6,6 +6,14 @@ pg.init()
 #Create the screen with 800x600 resolution
 screen = pg.display.set_mode((800, 600))
 
+# Title for the game
+pg.display.set_caption(" Samuel - Space Invaders")
+
+#Icon for the game
+icon = pg.image.load("assets/gps_icon32.png")
+pg.display.set_icon(icon)
+
+
 # Game loop
 running = True 
 while running:
@@ -13,5 +21,9 @@ while running:
         if event.type == pg.QUIT:    
             running = False # if we press the quit button the running goes to False and the screenis
 
+#   Define the color of the screen - RGB color
+    screen.fill((19 , 0, 77))
+#   to work properly we need to add a update function
+    pg.display.update()
 
 
